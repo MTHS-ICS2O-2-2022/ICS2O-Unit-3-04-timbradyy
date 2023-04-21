b452753dc1338a5236c2161f9ebb9c03e09c2eda
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-04-21 17:26:25 +0000

    doney

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tim
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates volume of a sphere.
+ */
+function calculate() {
+  // input
+  const fahrenheit = parseInt(document.getElementById("fahrenheit").value)
+
+  // process
+  const celsius = (fahrenheit - 32) * 5/9
+
+
+  // output
+  document.getElementById("celsius").innerHTML = "Celsius is: " + celsius.toFixed(2) + " °C."
+}
